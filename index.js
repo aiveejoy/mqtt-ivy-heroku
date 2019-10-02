5ead34b53be86d5a64fe0a09a0ea20d4c4f3708a
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-10-02 14:14:58 +0800

    na commit nako

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var express= require('express');
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
